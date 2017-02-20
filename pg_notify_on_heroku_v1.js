f808219cc5fe930192029a72255d7b4bbd324988
Shyam <kyabathais.com>
2017-02-20 23:19:55 +0530

    pg

--- pg_notify_on_heroku_v1.js
+++ pg_notify_on_heroku_v1.js
@@ -1,7 +1,7 @@
 var pg = require ('pg');
 
-var pgConString = "postgres://postgres:root@localhost/node_hero"
-
+var pgConString = "postgres://wnloxtbfrbjnae:c129f387c6659dd0f9033f00c94c24ea77af9735db7fad916dd011a95b567e30@ec2-23-21-184-181.compute-1.amazonaws.com:5432/d63icc9v8kpsii"
+//pg.defaults.ssl = true;
 pg.connect(pgConString, function(err, client) {
   if(err) {
     console.log(err);

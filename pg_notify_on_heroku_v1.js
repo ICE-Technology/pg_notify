dd7fff892947a6bb6fe0b50516beb02fc12c858f
Shyam <kyabathais.com>
2017-02-21 18:47:28 +0530

    pg

--- pg_notify_on_heroku_v1.js
+++ pg_notify_on_heroku_v1.js
@@ -1,5 +1,5 @@
 var pg = require ('pg');
-
+var app =express();
 const PORT = process.env.PORT || 3000;
 var pgConString = "postgres://wnloxtbfrbjnae:c129f387c6659dd0f9033f00c94c24ea77af9735db7fad916dd011a95b567e30@ec2-23-21-184-181.compute-1.amazonaws.com:5432/d63icc9v8kpsii"
 //var pgConString=process.env.DATABASE_URL;

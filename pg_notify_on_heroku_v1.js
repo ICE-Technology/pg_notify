ac6b0c4b31fc3a3146ca43cea0a10ffbb01efa7a
Shyam <kyabathais.com>
2017-02-21 18:44:39 +0530

    pg

--- pg_notify_on_heroku_v1.js
+++ pg_notify_on_heroku_v1.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 var pgConString = "postgres://wnloxtbfrbjnae:c129f387c6659dd0f9033f00c94c24ea77af9735db7fad916dd011a95b567e30@ec2-23-21-184-181.compute-1.amazonaws.com:5432/d63icc9v8kpsii"
 //var pgConString=process.env.DATABASE_URL;
 //pg.defaults.ssl = true;
-pg_notify_on_heroku_v1.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
 pg.connect(pgConString, function(err, client) {

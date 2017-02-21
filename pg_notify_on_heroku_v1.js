a88f5d23023594dac518f2f188ed5f361cddca11
Shyam <kyabathais.com>
2017-02-21 19:33:18 +0530

    pg

--- pg_notify_on_heroku_v1.js
+++ pg_notify_on_heroku_v1.js
@@ -1,7 +1,11 @@
 var pg = require ('pg');
-var app =express();
+
+var express = require('express');
+
+var app = express();
 const PORT = process.env.PORT || 3000;
-var pgConString = "postgres://wnloxtbfrbjnae:c129f387c6659dd0f9033f00c94c24ea77af9735db7fad916dd011a95b567e30@ec2-23-21-184-181.compute-1.amazonaws.com:5432/d63icc9v8kpsii"
+var pgConString = "postgres://postgres:root@localhost/node_hero";
+//var pgConString = "postgres://wnloxtbfrbjnae:c129f387c6659dd0f9033f00c94c24ea77af9735db7fad916dd011a95b567e30@ec2-23-21-184-181.compute-1.amazonaws.com:5432/d63icc9v8kpsii"
 //var pgConString=process.env.DATABASE_URL;
 //pg.defaults.ssl = true;
 app.listen(PORT, () => {
